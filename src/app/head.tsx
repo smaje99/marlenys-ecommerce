ceed0928143b04ba023c97fd4b8a1767c9384416
Sergio Majé <smajefranco@gmail.com>
2023-03-21 01:46:02 -0500

    💄 Set app theme color

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
             <title>Lencería Marlenys | Tienda virtual</title>
             <meta name='description' content='Tienda virtual Lencería Marlenys' />
             <meta name='author' content='Sergio Andrés Majé Franco' />
+            <meta name='theme-color' content='#FEEEF0' />
             <link rel='author' href='https://twitter.com/smaje99' />
         </>
     )
